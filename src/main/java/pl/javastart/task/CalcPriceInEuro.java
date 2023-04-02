adee9b2a97f2d98b15ed294152c16d84a83a6d41
kukers007 <96911087+kukers007@users.noreply.github.com>
2023-04-02 21:41:04 +0200

    Solution 13.3 poprawka 1

---
+++ src/main/java/pl/javastart/task/CalcPriceInEuro.java
@@ -0,0 +1,63 @@
+package pl.javastart.task;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalcPriceInEuro {
+
+    static BigDecimal getMinPrice(List<BigDecimal> result) {
+        if (result.isEmpty()) {
+            return null;
+        }
+        BigDecimal min = result.get(0);
+        for (int i = 1; i < result.size(); i++) {
+            if (result.get(i).compareTo(min) < 0) {
+                min = result.get(i);
+            }
+        }
+        return min;
+    }
+
+    static BigDecimal getMaxPrice(List<BigDecimal> result) {
+        if (result.isEmpty()) {
+            return null;
+        }
+        BigDecimal max = result.get(0);
+        for (int i = 1; i < result.size(); i++) {
+            if (result.get(i).compareTo(max) > 0) {
+                max = result.get(i);
+            }
+        }
+        return max;
+    }
+
+    static BigDecimal sumPriceInEuro(List<BigDecimal> prices) {
+        BigDecimal sumPriceInEuro = BigDecimal.ZERO;
+        for (BigDecimal price : prices) {
+            sumPriceInEuro = sumPriceInEuro.add(price);
+        }
+        return sumPriceInEuro;
+    }
+
+    static List<BigDecimal> priceInEuro(List<Product> products, List<Currency> currencies) {
+        List<BigDecimal> listPriceInEuro = new ArrayList<>();
+        for (Product product : products) {
+            Currency productCurrency = findCurrencyByCode(product.getCurrency(), currencies);
+            assert productCurrency != null;
+            BigDecimal priceInEuro = product.getPrice().divide(productCurrency.getExchangeRate(), 20, RoundingMode.HALF_UP);
+            listPriceInEuro.add(priceInEuro);
+        }
+        return listPriceInEuro;
+    }
+
+    private static Currency findCurrencyByCode(String currencyCode, List<Currency> currencies) {
+        for (Currency currency : currencies) {
+            if (currencyCode.equals(currency.getCurrency())) {
+                return currency;
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,58 +1,26 @@
 package pl.javastart.task;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
-    @SuppressWarnings("checkstyle:LocalVariableName")
     public static void main(String[] args) {
         try {
             List<Product> products = FileUtils.readProductPriceAndCurrency("src/main/resources/products.csv");
             List<Currency> currencies = FileUtils.readCurrency("src/main/resources/currencies.csv");
-            BigDecimal sumPriceInEuro = getSumPriceInEuro(products, currencies);
+            List<BigDecimal> priceInEuro = CalcPriceInEuro.priceInEuro(products, currencies);
+            BigDecimal sumPriceInEuro = CalcPriceInEuro.sumPriceInEuro(priceInEuro);
+            BigDecimal minPrice = CalcPriceInEuro.getMinPrice(priceInEuro);
+            BigDecimal maxPrice = CalcPriceInEuro.getMaxPrice(priceInEuro);
             System.out.println("Suma wszsystkich produktow w €: " + sumPriceInEuro);
             System.out.println("Srednia cena produktu: " + sumPriceInEuro.divide(BigDecimal.valueOf(products.size()), RoundingMode.HALF_UP));
-            System.out.println("Najniższa cena: " + Collections.max(priceInEuro(products, currencies)));
-            System.out.println("Najwyższa cena: " + Collections.min(priceInEuro(products, currencies)));
-        } catch (FileNotFoundException e) {
-            System.err.println("nie ma pliku");
+            System.out.println("Najwyższa cena: " + maxPrice);
+            System.out.println("Najniższa cena: " + minPrice);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.err.println("Nie znaleziono pliku");
         }
-    }
-
-    private static BigDecimal getSumPriceInEuro(List<Product> products, List<Currency> currencies) {
-        BigDecimal priceInEuro;
-        BigDecimal sumPriceInEuro = BigDecimal.ZERO;
-        for (Product product : products) {
-            for (Currency currency : currencies) {
-                if (product.getCurrency().equals(currency.getCurrency())) {
-                    priceInEuro = product.getPrice().divide(currency.getExchangeRate(), 40, RoundingMode.HALF_UP);
-                    sumPriceInEuro = sumPriceInEuro.add(priceInEuro);
-                    break;
-                }
-            }
-        }
-        return sumPriceInEuro;
-    }
-
-    private static List<BigDecimal> priceInEuro(List<Product> products, List<Currency> currencies) {
-        BigDecimal priceInEuro = BigDecimal.ZERO;
-        List<BigDecimal> listPriceInEuro = new ArrayList<>();
-        for (Product product : products) {
-            for (Currency currency : currencies) {
-                if (product.getCurrency().equals(currency.getCurrency())) {
-                    priceInEuro = product.getPrice().divide(currency.getExchangeRate(), 20, RoundingMode.HALF_UP);
-                    listPriceInEuro.add(priceInEuro);
-                }
-            }
-        }
-        return listPriceInEuro;
     }
 }
